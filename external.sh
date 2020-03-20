544f4594e192deb18e5d722a45ddbd09e007b374
Cellebyte <marcel.fest@live.de>
2020-03-20 14:40:20 +0100

    Use a new release way for checking out submodules via git

---
+++ external.sh
@@ -0,0 +1,20 @@
+
+EXTERNAL_DIR=./external
+CHARTS_DIR=./charts
+for dir in $CHARTS_DIR/*/
+do
+    dir=${dir%*/}
+    directory=${dir##*/}
+    directory_version=`echo "${CHARTS_DIR}/${dir##*/}/version.txt"`
+    echo "${directory_version}"
+    if [ -f "$directory_version" ]; then
+        echo "${directory}"
+        echo "${directory_version}"
+        VERSION=`cat "${directory_version}"`
+        UNPREFIXED_VERSION=${VERSION}
+        UNPREFIXED_VERSION=${UNPREFIXED_VERSION#"v"}
+        git -C "${EXTERNAL_DIR}/${directory}" checkout "${VERSION}"
+        sed -i "s/^\(appVersion:\s*\).*/\1$UNPREFIXED_VERSION/" "${CHARTS_DIR}/${directory}/Chart.yaml"
+        sed -i "s/^\(version:\s*\).*/\1$UNPREFIXED_VERSION/" "${CHARTS_DIR}/${directory}/Chart.yaml"
+    fi
+done
